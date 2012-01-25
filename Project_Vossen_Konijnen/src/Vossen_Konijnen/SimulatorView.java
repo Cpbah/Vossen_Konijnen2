39af04ec98c2ede2b15012d864cede49078afd93
Steffan Oldenburger <steffan_oldenburger@hotmail.com>
2012-01-25 12:02:00 +0100

    Version 1.4_

--- Project_Vossen_Konijnen/src/Vossen_Konijnen/SimulatorView.java
+++ Project_Vossen_Konijnen/src/Vossen_Konijnen/SimulatorView.java
@@ -193,6 +193,10 @@ public class SimulatorView implements ActionListener
         return stats.isViable(field);
     }
     
+    /**
+     * Setter voor Simulator
+     * @param simulator1
+     */
     public static void setSimulator(Simulator simulator1) {
 		simulator = simulator1;
 	}
